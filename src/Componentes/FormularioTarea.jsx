4822a8d637af06b23c4b1407d8d185a7e3704225
harol <haroldhdiaz@gmail.com>
2025-08-14 14:32:46 -0600

    Component not recognized

--- src/Componentes/FormularioTarea.jsx
+++ src/Componentes/FormularioTarea.jsx
@@ -1,3 +1,24 @@
-import { React, useState } from "react";
+import { React, use, useState } from "react";
 
-function FormularioTarea() {}
+function FormularioTarea() {
+  const [agregarTarea, setAgregarTarea] = useState([]);
+  const [newTarea, setNewTarea] = useState("");
+
+  <div className="Lista-Tareas">
+    <header className="Header">
+      <h1>Lista de Tareas 📃🏡</h1>
+    </header>
+
+    <div className="Formulario">
+      <input
+        type="text"
+        value={newTarea}
+        onChange={(e) => setNewTarea(e.target.value)}
+        placeholder="Escribir una tarea..."
+      />
+      <button className="Boton-Agregar" onClick={agregarTarea}>
+        Agregar
+      </button>
+    </div>
+  </div>;
+}
